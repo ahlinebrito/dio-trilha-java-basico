31ef29ade20091e9a9e80b7dcf8a94dc9f677eb1
alinebsimas <alinebrito.ich@gmail.com>
2023-02-19 12:54:17 -0300

    estruturas de repetição

---
+++ exercicios-loops-dio/src/Exerciciosloops.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/*Faça um programa que leia 5 números e informe o maior número e a média desses números. */
+
+
+public class Exerciciosloops {
+    
+public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    int numero;
+    int maior = 0;
+
+    int count = 0;
+
+do {
+    System.out.println("Numero: ");
+    numero = scan.nextInt();
+
+    if (numero > maior) maior = numero;
+
+    count = count + 1;
+    System.out.println("Maior: " + maior);
+
+ 
+}  while(count < 5);
+
+
+
+}
+
+    }
+
