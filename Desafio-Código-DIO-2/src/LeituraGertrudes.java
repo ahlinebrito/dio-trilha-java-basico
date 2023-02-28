69c23c2e41ccc92e6e0ad496d9f3697007a23ae2
alinebsimas <alinebrito.ich@gmail.com>
2023-02-28 17:04:04 -0300

    Leitura da Gertrudes

---
+++ Desafio-Código-DIO-2/src/LeituraGertrudes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+    
+public class LeituraGertrudes {
+    public static void main(String[] args) {
+        try (Scanner leitor = new Scanner(System.in)) {
+            int paginas = leitor.nextInt();
+            int paginasLidas = 3;
+            int numeroDias = paginas / paginasLidas; 
+            
+            System.out.println(numeroDias + " dias");
+        } 
+        
+    
+    }
+}
